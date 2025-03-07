0c9f18b08dd8f249d8b3611a1a98a05a243293d7
Sebastian Rodriguez Ardila <sebasplay2@hotmail.com>
2025-03-06 22:23:59 -0500

    Add url for whatssap chat

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from pruebapp.views import ReunionViewSet  # Importa la vista correctamente
+from pruebapp.views import ReunionViewSet
+from pruebapp.views import whatsapp_webhook
 
-# Crea un router y registra la vista
 router = DefaultRouter()
 router.register(r'reuniones', ReunionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # Incluye las rutas de DRF
+    path('api/', include(router.urls)),
+    path('api/whatsapp-webhook/', whatsapp_webhook, name='whatsapp_webhook'),
 ]
